ecdb8ca87b5af8651315977aea55507d8f70c933
Maynot2 <paulmanot@gmail.com>
2021-09-03 10:30:10 +0200

    Another test

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -18,7 +18,7 @@
 
 					<div class="footer-credits">
 
-						<p class="footer-copyright">hello from ssm test 2 
+						<p class="footer-copyright">hello from ssm test 3 
 							<?php
 							echo date_i18n(
 								/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
