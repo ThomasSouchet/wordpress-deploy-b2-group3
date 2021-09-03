f8ab511a978a1de69593b386c3694c24bc2d931e
Thomas Souchet <thomas.souchet@gmail.com>
2021-09-03 11:35:43 +0200

    Modify db config script

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -3,6 +3,8 @@ user=$(aws ssm get-parameter --name user --output text --query Parameter.Value)
 password=$(aws ssm get-parameter --name password --output text --query Parameter.Value)
 entrypoint=$(aws ssm get-parameter --name entrypoint --output text --query Parameter.Value)
 
+sudo chown -R www-data: /srv/www
+
 sudo -u www-data sed -i "s/database_name_here/$db/" /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i "s/username_here/$user/" /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i "s/password_here/$password/" /srv/www/wordpress/wp-config.php
