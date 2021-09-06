d5e5e86430804dfb31d603c1a216df4e899bac1a
Thomas Souchet <thomas.souchet@gmail.com>
2021-09-06 14:46:44 +0200

    Modify again dbconfig script

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -10,9 +10,9 @@ echo $entrypoint
 
 sudo chown -R www-data: /srv/www
 
-sudo -u www-data -i -- wp config set DB_NAME "$db" --path="/srv/www/wordpress"
-sudo -u www-data -i -- wp config set DB_USER "$user" --path="/srv/www/wordpress"
-sudo -u www-data -i -- wp config set DB_PASSWORD "$password" --path="/srv/www/wordpress"
-sudo -u www-data -i -- wp config set DB_HOST "$entrypoint" --path="/srv/www/wordpress"
+runuser -l www-data -c 'wp config set DB_NAME "$db" --path="/srv/www/wordpress"'
+runuser -l www-data -c 'wp config set DB_USER "$user" --path="/srv/www/wordpress"'
+runuser -l www-data -c 'wp config set DB_PASSWORD "$password" --path="/srv/www/wordpress"'
+runuser -l www-data -c 'wp config set DB_HOST "$entrypoint" --path="/srv/www/wordpress"'
 
 sudo service apache2 reload
